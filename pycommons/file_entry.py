0ed13de225a425f6e3049dee81fafd1b558d633d
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-04-01 14:18:39 -0400

    Added pycommons     - file_entry.py     - shutil_helper.py

---
+++ pycommons/file_entry.py
@@ -0,0 +1,56 @@
+import os
+import shutil_helper
+
+class FileEntry(list):
+    def __init__(self, name, parent):
+        if parent is not None and not isinstance(parent, FileEntry):
+            print "Cannot initialize class FileEntry with parent of type '%s'" % (parent.__class__)
+            raise Exception
+        super(FileEntry, self).__init__()
+        self.name    = name
+        self.parent   = parent
+
+    def build(self, regex='*.*'):
+        files, dirs = shutil_helper.ls(self.path(), regex)
+        entries = list(files)
+        entries.extend(dirs)
+        # Append the current path since ls only returns basenames
+        entries = [os.path.join(self.path(), e) for e in entries]
+        for entry in entries:
+            basename = os.path.basename(entry)
+            dirname  = os.path.dirname(entry)
+            assert os.path.abspath(self.path()) == os.path.abspath(dirname), 'does not match:\nself   :%s\ndirname:%s' % (self.path(), dirname)
+            child = FileEntry(basename, self)
+            self.append(child)
+            if os.path.isdir(entry):
+                child.build(regex)
+
+    def append(self, o):
+        # FIXME: Very inefficient to sort on every append()
+        super(FileEntry, self).append(o)
+        self.sort(key=lambda x: x.name)
+
+    def path(self):
+        if self.parent is not None:
+            return os.path.join(self.parent.path(), self.name)
+        else:
+            return self.name
+
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return '%s -> %s' % (self.name, super(FileEntry, self).__repr__())
+
+    @staticmethod
+    def prefix_tabs(name, tabs):
+        retval = ''
+        for i in range(tabs):
+            retval += '\t'
+        retval += name
+        return retval
+
+    def pretty_print(self, tabs=0):
+        print FileEntry.prefix_tabs(self.name, tabs)
+        for child in self:
+            child.pretty_print(tabs + 1)
+
---
+++ pycommons/shutil_helper.py
@@ -0,0 +1,48 @@
+import os
+import fnmatch
+import shutil
+import json
+
+def recursive_copy(base_dir, src, dest):
+	cwd = os.getcwd()
+	os.chdir(base_dir)
+	try:
+		shutil.copytree(src, dest)
+	except OSError as exc:
+		if exc.errno == errno.ENOTDIR:
+			shutil.copy(src, dest)
+		else:
+			raise
+	os.chdir(cwd)
+
+def recursive_list(src, regex):
+	files = []
+	for root, dirnames, filenames in os.walk(src):
+		for filename in fnmatch.filter(filenames, regex):
+			files.append(os.path.abspath(os.path.join(root, filename)))
+		for dirname in dirnames:
+			path = os.path.join(root, dirname)
+			for entry in recursive_list(path, regex):
+				files.append(entry)
+	files = list(set(files))
+	files.sort()
+	return files
+
+'''
+Only applies regex to files
+'''
+def ls(src, regex):
+	files = []
+	dirs  = []
+	entries = os.listdir(src)
+	for entry in entries:
+		path = os.path.join(src, entry)
+		if os.path.isfile(path) and fnmatch.fnmatch(entry, regex):
+			files.append(entry)
+		if os.path.isdir(path):
+			dirs.append(entry)
+
+	files.sort()
+	dirs.sort()
+	return files, dirs
+
