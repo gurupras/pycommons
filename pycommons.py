2859ec0aaf2ebcaa1f2d7b93c2cc70508e3f5ebd
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-09-11 18:07:29 -0400

    Added pycommons.py

---
+++ pycommons.py
@@ -0,0 +1,24 @@
+import subprocess
+import logging
+
+import generic_logging
+logger = logging.getLogger()
+
+def run(cmd,
+		stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+		log=False, fail_on_error=True):
+	logger.info('$ ' + cmd)
+	p = subprocess.Popen(cmd, shell=True, stdout=stdout, stderr=stderr)
+	p.wait()
+
+	stdout, stderr = p.communicate()
+	if log:
+		logger.debug(stdout)
+		logger.warning(stderr)
+
+	if p.returncode != 0:
+		logger.warning('Process returned: %d' % (p.returncode))
+		if fail_on_error:
+			raise Exception('%s: %d' % (cmd, p.returncode))
+	return p.returncode, stdout, stderr
+
