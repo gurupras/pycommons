0b68cf0b5a100ec75f645a5efb683fab6de34b05
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-09-22 21:37:36 -0400

    Fixed bug in generic_logging

--- generic_logging.py
+++ generic_logging.py
@@ -4,7 +4,7 @@ __LOGGING_FORMAT = "[%(filename)s:%(lineno)s - %(funcName)10s() ]: %(message)s"
 __FILE_FORMAT = "%(message)s"
 
 def init(level, filename=None):
-	if file:
+	if filename:
 		logging.basicConfig(filename=filename, filemode='w', format=__FILE_FORMAT, level=level)
 	else:
 		logging.basicConfig(format=__LOGGING_FORMAT, level=level)
