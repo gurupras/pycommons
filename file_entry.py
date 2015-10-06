94cfeb75174aac80a2ea30ff154858d742073be9
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-10-06 07:26:55 -0400

    Replaced spaces with tabs in file_entry.py

--- file_entry.py
+++ file_entry.py
@@ -2,77 +2,77 @@ import os
 import shutil_helper
 
 class FileEntry(list):
-    def __init__(self, name, parent):
-        if parent is not None and not isinstance(parent, FileEntry):
-            print "Cannot initialize class FileEntry with parent of type '%s'" % (parent.__class__)
-            raise Exception
-        super(FileEntry, self).__init__()
-        self.name    = name
-        self.parent   = parent
+	def __init__(self, name, parent):
+		if parent is not None and not isinstance(parent, FileEntry):
+			print "Cannot initialize class FileEntry with parent of type '%s'" % (parent.__class__)
+			raise Exception
+		super(FileEntry, self).__init__()
+		self.name	= name
+		self.parent   = parent
 
-    def build(self, regex=['*.*']):
-        del self[:]
-        path = self.path()
-        if os.path.isdir(path):
-            files, dirs = shutil_helper.ls(path, regex)
-            entries = list(files)
-            entries.extend(dirs)
-            # Append the current path since ls only returns basenames
-            entries = [os.path.join(path, e) for e in entries]
-            for entry in entries:
-                basename = os.path.basename(entry)
-                dirname  = os.path.dirname(entry)
-                assert os.path.abspath(path) == os.path.abspath(dirname), \
-                        'does not match:\nself   :%s\ndirname:%s' % ( \
-                        path, dirname)
-                child = FileEntry(basename, self)
-                self.append(child)
-                if os.path.isdir(entry):
-                    child.build(regex)
+	def build(self, regex=['*.*']):
+		del self[:]
+		path = self.path()
+		if os.path.isdir(path):
+			files, dirs = shutil_helper.ls(path, regex)
+			entries = list(files)
+			entries.extend(dirs)
+			# Append the current path since ls only returns basenames
+			entries = [os.path.join(path, e) for e in entries]
+			for entry in entries:
+				basename = os.path.basename(entry)
+				dirname  = os.path.dirname(entry)
+				assert os.path.abspath(path) == os.path.abspath(dirname), \
+						'does not match:\nself   :%s\ndirname:%s' % ( \
+						path, dirname)
+				child = FileEntry(basename, self)
+				self.append(child)
+				if os.path.isdir(entry):
+					child.build(regex)
 
-    def append(self, o):
-        # FIXME: Very inefficient to sort on every append()
-        super(FileEntry, self).append(o)
-        self.sort(key=lambda x: x.name)
+	def append(self, o):
+		# FIXME: Very inefficient to sort on every append()
+		super(FileEntry, self).append(o)
+		self.sort(key=lambda x: x.name)
 
-    def path(self):
-        if self.parent is not None:
-            return os.path.join(self.parent.path(), self.name)
-        else:
-            return self.name
+	def path(self):
+		if self.parent is not None:
+			return os.path.join(self.parent.path(), self.name)
+		else:
+			return self.name
 
-    def root(self):
-        if self.parent is None:
-            return self
-        else:
-            return self.parent.root()
+	def root(self):
+		if self.parent is None:
+			return self
+		else:
+			return self.parent.root()
 
-    def __str__(self):
-        return self.name
-    def __repr__(self):
-        if os.path.isfile(self.path()):
-            return '%s' % (self.name)
-        else:
-            return '%s -> %s' % (self.name, super(FileEntry, self).__repr__())
+	def __str__(self):
+		return self.name
+	def __repr__(self):
+		if os.path.isfile(self.path()):
+			return '%s' % (self.name)
+		else:
+			return '%s -> %s' % (self.name, super(FileEntry, self).__repr__())
 
-    def isdir(self):
-        path = self.path()
-        return os.path.isdir(path)
+	def isdir(self):
+		path = self.path()
+		return os.path.isdir(path)
 
-    def isfile(self):
-        path = self.path()
-        return os.path.isfile(path)
+	def isfile(self):
+		path = self.path()
+		return os.path.isfile(path)
 
-    @staticmethod
-    def prefix_tabs(name, tabs):
-        retval = ''
-        for i in range(tabs):
-            retval += '\t'
-        retval += name
-        return retval
+	@staticmethod
+	def prefix_tabs(name, tabs):
+		retval = ''
+		for i in range(tabs):
+			retval += '\t'
+		retval += name
+		return retval
 
-    def pretty_print(self, tabs=0):
-        print FileEntry.prefix_tabs(self.name, tabs)
-        for child in self:
-            child.pretty_print(tabs + 1)
+	def pretty_print(self, tabs=0):
+		print FileEntry.prefix_tabs(self.name, tabs)
+		for child in self:
+			child.pretty_print(tabs + 1)
 
